fcd3f9003644895f7b6d83bcb83a561b39a15930
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2025-11-27 19:01:42 +0500

    MaterialMongoDB

--- backend/src/Infrastructure/FIIT-folder.Infrastructure/FileStorage/MaterialMongoDB.cs
+++ backend/src/Infrastructure/FIIT-folder.Infrastructure/FileStorage/MaterialMongoDB.cs
@@ -1,23 +1,57 @@
 ﻿using FIIT_folder.Domain.Interfaces;
 using FIIT_folder.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;  
 
 namespace FIIT_folder.Infrastructure.FileStorage;
 
 public class MaterialMongoDB : IMaterialMongoDB
 {
+    private readonly IMongoCollection<BsonDocument> Materials;
+
+    public MaterialMongoDB(IConfiguration configuration)
+    {
+        var connectionString = configuration["MongoDbSettings:ConnectionString"];
+        var databaseName = configuration["MongoDbSettings:DatabaseName"];
+            
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase(databaseName);
+        Materials = database.GetCollection<BsonDocument>("Materials");
+    }
+    
+    public async Task<Material> CreateMaterial(Material material)
+    {
+        try
+        {
+            Console.WriteLine("Сохраняю материал");
+            
+            var document = new BsonDocument //пока обычный словарик чтобы просто проверить
+            {
+                { "materialId", material.Id.ToString() },
+                { "name", material.Name },
+                { "path", material.Path },
+                { "size", material.Size },
+                { "type", material.Type },
+                { "date", DateTime.UtcNow },
+                { "createdAt", DateTime.UtcNow }
+            };
+
+            await Materials.InsertOneAsync(document);
+            return material;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Ошибка в сохранении файла");
+        }
+    }
     
     public Task<Material> GetByIdMaterial(string id)
     {
         throw new NotImplementedException();
-        var a = "mongodb+srv://admin:<db_password>@cluster0.rzsw1qe.mongodb.net/?appName=Cluster0";
     }
 
     public Task<bool> DeleteMaterial(string id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Material> CreateMaterial(Material material)
     {
         throw new NotImplementedException();
     }
---
+++ backend/src/Infrastructure/FIIT-folder.Infrastructure/TesterMongoBD.cs
@@ -0,0 +1,30 @@
+﻿using FIIT_folder.Domain.Models;
+using FIIT_folder.Domain.Interfaces;
+using FIIT_folder.Infrastructure.FileStorage;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FIIT_folder.Infrastructure.Test;
+
+public class TesterMongoBD
+{
+    public async Task CreateMaterial()
+    {
+        
+    }
+    
+    public async Task DeleteMaterial()
+    {
+        
+    }
+    
+    public async Task GetMaterial()
+    {
+        
+    }
+    
+    public async Task IsMaterialInMongoDB()
+    {
+        
+    }
+}
