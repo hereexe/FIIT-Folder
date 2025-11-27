aa8de195a2ce6d25a03919ad4a94b0f0eb6f7852
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2025-11-27 18:06:36 +0500

    Реализация интерфейса

---
+++ backend/src/Infrastructure/FIIT-folder.Infrastructure/FileStorage/MaterialMongoDB.cs
@@ -0,0 +1,23 @@
+﻿using FIIT_folder.Domain.Interfaces;
+using FIIT_folder.Domain.Models;
+
+namespace FIIT_folder.Infrastructure.FileStorage;
+
+public class MaterialMongoDB : IMaterialMongoDB
+{
+    
+    public Task<Material> GetByIdMaterial(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteMaterial(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Material> CreateMaterial(Material material)
+    {
+        throw new NotImplementedException();
+    }
+}
