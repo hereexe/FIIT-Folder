93e8f4fbe367c48f3e1f93a9b85aff6ed03c534e
Ravil <182143436+hereexe@users.noreply.github.com>
2025-11-30 20:58:08 +0500

    for test branch

--- backend/src/Core/FIIT-folder.Domain/Interfaces/IFileStorageRepository.cs
+++ backend/src/Core/FIIT-folder.Domain/Interfaces/IFileStorageRepository.cs
@@ -3,7 +3,7 @@
 public interface IFileStorageRepository
 {
     Task <string> SaveFile(string name, long size, string type, Stream content, string path); //возвращает полный путь к файлу
-    Task<Stream> GetFile(string fullPathFile); //Task вып асинхроно
+    Task<Stream> GetFile(string fullPathFile); //Task вып асинхроно.
     
     Task DeleteFile(string fullPathFile);
     
