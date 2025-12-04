d69536c021a832c52a9d7f3b42d1f66c06d21ba2
Fedor <ftepka20062006@gmail.com>
2025-12-04 16:40:31 +0500

    rtk query

--- frontend/api/types.ts
+++ frontend/api/types.ts
@@ -1,3 +1,11 @@
+export interface AuthResponse {
+  token: string;
+}
+
+export interface RegisterResponse {
+  message: string;
+}
+
 export interface MaterialDto {
   id: string;
   name: string;
@@ -6,17 +14,23 @@ export interface MaterialDto {
 
 export interface UploadResponse {
   id: string;
-  subject: string;
 }
 
 export interface DownloadLinkResponse {
   downloadUrl: string;
 }
 
-export interface AuthResponse {
-  token: string;
+export interface SubjectDto {
+  id: string;
+  name: string;
 }
 
-export interface RegisterResponse {
-  message: string;
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface RegisterRequest {
+  email: string;
+  password: string;
 }
