3e4ae0870a7b4caae5132ad5f92dfd2cc1b874ee
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2025-12-11 20:39:59 +0500

    Test Cloud for Pasha the Looser

--- backend/src/Infrastructure/FIIT-folder.Infrastructure/Tests_demo/FileStorageRepository_Should.cs
+++ backend/src/Infrastructure/FIIT-folder.Infrastructure/Tests_demo/FileStorageRepository_Should.cs
@@ -1,28 +1,32 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using System.Text;
-using FIIT_folder.Domain.Interfaces; 
-using NUnit.Framework;
-using FIIT_folder.Infrastructure.FileStorage;
-
-namespace TestsAll.Tests.Infrastructure;
-
-[TestFixture]
-public class FileStorageRepository_Should
-{
-    [Test]
-    public void Test_Config_Loads()
-    {
-        // Пример теста для Infrastructure
-        Assert.That(1 + 1, Is.EqualTo(2));
-    }
-        
-    [Test]
-    public void Test_MongoDB_ConnectionString_Format()
-    {
-        // Тестируем что-то из вашего Infrastructure
-        string connectionString = "mongodb://localhost:27017";
-        Assert.That(connectionString, Does.Contain("mongodb://"));
-    }
-}
+﻿// using System;
+// using System.IO;
+// using System.Threading.Tasks;
+// using System.Text;
+// using FIIT_folder.Domain.Interfaces; 
+// using NUnit.Framework;
+// using FIIT_folder.Infrastructure.FileStorage;
+//
+// namespace TestsAll.Tests.Infrastructure;
+//
+// [TestFixture]
+// public class FileStorageRepository_Should
+// {
+//     [Test]
+//     // public void Test1_YandexCloud_True_Connection()
+//     // {
+//     //     Assert.That(new FileStorageRepository(
+//     //         "YCAJEJjZUAxs4F0iCpajJG4_L",
+//     //         "YCNbbq1t3RGwiRuNrNAnCsODmPVgWFM1s6jT201L",  
+//     //         "my-fiit",
+//     //         "ru-central1"), 
+//     //     );
+//     // }
+//         
+//     [Test]
+//     public void Test2_MongoDB_ConnectionString_Format()
+//     {
+//         // Тестируем что-то из вашего Infrastructure
+//         string connectionString = "mongodb://localhost:27017";
+//         Assert.That(connectionString, Does.Contain("mongodb://"));
+//     }
+// }
