9389d587f7039144252c89fff6a8785711810a0d
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2025-12-20 20:38:30 +0500

    Che to zarabotalo&!!!&!&

---
+++ backend/src/Infrastructure/FIIT-folder.Infrastructure/Tests/FIIT-folder.Infrastructure.Tests/StudyMaterial_Should.cs
@@ -0,0 +1,58 @@
+﻿// using System;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using FIIT_folder.Domain.Entities;
+// using FIIT_folder.Domain.Enums;
+// using FIIT_folder.Domain.Value_Object;
+// using FIIT_folder.Infrastructure.FileStorage;
+// using NUnit.Framework;
+//
+// namespace FIIT_folder.Infrastructure.Tests;
+//
+// [TestFixture]
+// public class Basic_Should
+// {
+//     [Test]
+//     public void TwoPlusTwoEqualsFour()
+//     {
+//         Assert.That(2 + 2, Is.EqualTo(4));
+//     }
+// }
+//
+// // [TestFixture]
+// // public class StudyMaterialMongoDB_Should
+// // {
+// //     private StudyMaterialMongoDB mongo;
+// //
+// //     // [SetUp]
+// //     // public void SetUp()
+// //     // {
+// //     //     mongo = mongo.Start();
+// //     //     repository = new StudyMaterialMongoDB(
+// //     //         mongo.ConnectionString,
+// //     //         "StudyMaterialTestDb"
+// //     //     );
+// //     // }
+// //     //
+// //     // [TearDown]
+// //     // public void TearDown()
+// //     // {
+// //     //     mongo.Dispose();
+// //     // }
+// // }    
+
+
+
+using NUnit.Framework;
+
+namespace FIIT_folder.Infrastructure.Tests;
+
+[TestFixture]
+public class StudyMaterial_Should
+{
+    [Test]
+    public void TwoPlusTwoEqualsFour()
+    {
+        Assert.That(2 + 2, Is.EqualTo(4));
+    }
+}
--- backend/src/Infrastructure/FIIT-folder.Infrastructure/Tests/MongoDB/StudyMaterial_Should.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FIIT_folder.Infrastructure.Tests;
-
-public class StudyMaterial_Should
-{
-    
-}
