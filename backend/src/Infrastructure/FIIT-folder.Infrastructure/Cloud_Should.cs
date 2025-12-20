ec6808fa6616981201839ea7f5ac21ae2b425b73
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2025-12-20 22:08:13 +0500

    for server

---
+++ backend/src/Infrastructure/FIIT-folder.Infrastructure/Cloud_Should.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using FIIT_folder.Infrastructure.FileStorage;
+using FIIT_folder.Domain.Entities;
+using FIIT_folder.Domain.Value_Object;
+using MongoDB.Driver;
+using FIIT_folder.Domain.Enums;
+using System.Threading.Tasks;
+
+namespace FIIT_folder.Infrastructure.Tests;
+
+public class Cloud_Should
+{
+    
+}
