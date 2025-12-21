69cfaa93fd6a0c1f295fedda6225198a42045686
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2025-12-21 18:02:46 +0500

    ZARABOTALO ALELUYA!!!

---
+++ backend/src/FIIT-folder.Tests/SimpleMathTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FIIT_folder.Tests
+{
+    public class SimpleMathTest
+    {
+        [Fact]
+        public void Two_Plus_Two_Equals_Four()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
