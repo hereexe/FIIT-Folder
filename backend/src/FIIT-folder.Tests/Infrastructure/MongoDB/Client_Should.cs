e73bbe2327b8fb17856ab05392e07be6202f9f77
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2025-12-21 18:18:02 +0500

    Add new folder and first tests

---
+++ backend/src/FIIT-folder.Tests/Infrastructure/MongoDB/Client_Should.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using FIIT_folder.Infrastructure.FileStorage;
+
+namespace FIIT_folder.Tests
+{
+    public class Client_Should
+    {
+        [Fact]
+        public void Two_Plus_Two_Equals_Four()
+        {
+            Assert.Equal(5, 3 + 2);
+        }
+    }
+}
